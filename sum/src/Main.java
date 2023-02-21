5cb905ebea32487f358109afb57c8da485907256
asbhatti <asbhatti.in@gmail.com>
2023-02-21 12:14:50 +0530

    adding temp.java and Diff.java

--- sum/src/Main.java
+++ sum/src/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         int a = 5;
         int b = 6;
-        System.out.println(a+b);
+        System.out.println("The sum is: " + (a+b));
     }
 
 }
